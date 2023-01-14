b0eda000f0517d6cfd62791e21f3180282ea6eb7
ijasgit <mohammedijasn3899@gmail.com>
2023-01-14 23:08:22 +0530

    .

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 import "./Registration/Register";
 import Register from "./Registration/Register";
 import { Route, Routes } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter,HashRouter } from "react-router-dom";
 import Login from "./login/Login";
 import UserDetails from "./employee details/UserDetails";
 import AddEmployee from "./addemployee/AddEmployee";
@@ -11,16 +11,16 @@ import View from "./View";
 function App() {
   return (
     <div>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="https://ijasgit.github.io/employee-details/ " element={<Register />}></Route>
-          <Route path="https://ijasgit.github.io/employee-details/login" element={<Login />}></Route>
-          <Route path="https://ijasgit.github.io/employee-details/userdetails" element={<UserDetails />}></Route>
-          <Route path="https://ijasgit.github.io/employee-details/view/:id" element={<View />}></Route>
-          <Route path="https://ijasgit.github.io/employee-details/addemployee" element={<AddEmployee />}></Route>
+          <Route path="/" element={<Register />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/userdetails" element={<UserDetails />}></Route>
+          <Route path="/view/:id" element={<View />}></Route>
+          <Route path="/addemployee" element={<AddEmployee />}></Route>
           <Route path="*" element={<h1>not found</h1>}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
